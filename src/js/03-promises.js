c03c14f7405f95101bef3683bc5539212c6d3c2b
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-07-08 23:49:34 +0300

    HW_JS_09_Task_2_v_17

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,58 +5,66 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', onFormInput);
 
-let formData = {
+const formData = {
   delay: '',
   step: '',
   amount: '',
 };
 
+const step = Number(formData.step);
+      
+function onFormSubmit(evt) {
+  evt.preventDefault();
 
-
-function onFormSubmit() {
+  let delay = Number(formData.delay);
+  let step = Number(formData.step);
   
-  const STEP = formData.step;
-  onFormInput();
-
-  setTimeout(() => {
+  for (let i = 1; i <= Number(formData.amount); i += 1) {
+    
+      const position = i;
+      
+    setTimeout(() => {
+      
+      delay += step; 
+     
     createPromise({ position, delay })
       .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-   
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`); 
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-    
       });
-  }, STEP);
 
+    }, delay);
+    
+  };  
   
  };
 
 function onFormInput(evt) { 
-  
  
   formData[evt.target.name] = evt.target.value;
-  console.log(formData);
-//  console.log(formData.delay);
-  return formData;
+
 };
 
 
 
 function createPromise({ position, delay }) {
-  return new Promise((res, rej) => {
-    const DELAY = delay;
+  
+  return new Promise((res, rej) => { 
+
     setTimeout(() => {
+
       const shouldResolve = Math.random() > 0.3;
+      
       if (shouldResolve) {
         res({ position, delay })
       } else {
         rej({ position, delay })
-      }
-    }, DELAY);
+       }
+    }, delay);
    
-  });
+  }); 
 };
 
 
